2987458f26c55f859a001913764d678c3d5e3fd1
Igor10111 <igormalcev10@icloud.com>
2024-11-17 20:51:29 +0300

    Add files via upload

---
+++ самост 6/sam6/sam6/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sam6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Настройка автозаполнения для textBoxSpecialty
+            SetupAutoComplete();
+        }
+
+        // Метод для настройки автозаполнения
+        private void SetupAutoComplete()
+        {
+            // Создаем коллекцию строк с названиями специальностей
+            var specialties = new AutoCompleteStringCollection
+            {
+                "Программирование в компьютерных системах",
+                "Информационные системы и программирование",
+                "Конструирование, моделирование и технологий швейных изделий",
+                "Конструирование, моделирование и технология изделий из кожи"
+            };
+
+            // Настройка TextBox для автозаполнения
+            textBox1.AutoCompleteCustomSource = specialties;                // Устанавливаем список специальностей
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;      // Показывать и дополнять текст
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;   // Используем пользовательский список
+        }
+    }
+}
